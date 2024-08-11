5790b26c6ee82c0fede7aa23d025596275b77e70
Agravlin <ceyhunemrebey@hotmail.com>
2024-08-11 13:05:26 +0200

    add: create a simple example

---
+++ src/bot.py
@@ -0,0 +1,27 @@
+import discord
+from config import DISCORD_TOKEN
+
+
+if __name__ == "__main__":
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        print(f'Logged in as {client.user}')
+
+    @client.event
+    async def on_message(message: discord.Message) -> None:
+        # Prevent recursive messages
+        if message.author == client.user:
+            return
+        
+        # Print the content
+        print(f'{message.author} wrote {message.content}')
+
+        # Starting letter
+        if message.content.startswith('.'):
+            await message.channel.send('Hello!')
+
+    client.run(token=DISCORD_TOKEN)
