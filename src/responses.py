cc7e95cc279becb34e89f8bea596f3d123e42188
Agravlin <ceyhunemrebey@hotmail.com>
2024-08-11 14:43:10 +0200

    feat: add volume logic and change to opus

--- src/responses.py
+++ src/responses.py
@@ -2,11 +2,13 @@ import discord
 import yt_dlp as youtube_dl
 import asyncio
 
+VOLUME_VALUE = 0.5
+
 ytdl_format_options = {
     'format': 'bestaudio/best',
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
+        'preferredcodec': 'opus',
         'preferredquality': '192',
     }],
     'noplaylist': True,
@@ -19,7 +21,7 @@ ffmpeg_options = {
 ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 
 class YTDLSource(discord.PCMVolumeTransformer):
-    def __init__(self, source, *, data, volume=0.5):
+    def __init__(self, source, *, data, volume=VOLUME_VALUE):
         super().__init__(source, volume)
         self.data = data
         self.title = data.get('title')
@@ -34,6 +36,8 @@ class YTDLSource(discord.PCMVolumeTransformer):
         return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
 
 async def handle_command(client, message: discord.Message) -> str:
+    global VOLUME_VALUE
+
     if message.content.startswith('.play '):
         url = message.content.split(' ')[1]
         if message.author.voice:
@@ -44,11 +48,21 @@ async def handle_command(client, message: discord.Message) -> str:
 
             async with message.channel.typing():
                 player = await YTDLSource.from_url(url, loop=client.loop, stream=True)
+                player.volume = VOLUME_VALUE
                 voice_client.play(player, after=lambda e: print(f'Player error: {e}') if e else None)
 
             return f'Now playing: {player.title}'
         else:
             return "You need to be in a voice channel to play music."
 
-    # Add more command handling logic here
-    return f'Now playing: {player.title}'
+    elif message.content.startswith('.volume'):
+        if len(message.content) == 7:
+            return f'Current Volume is: {VOLUME_VALUE}'
+        elif message.content[7:9] == " +" or " -":
+            if message.content[8] == "+":
+                VOLUME_VALUE += float(message.content[9:])
+            else:
+                VOLUME_VALUE -= float(message.content[9:])
+        VOLUME_VALUE = max(0.0, min(VOLUME_VALUE, 2.0))
+
+    return f"Volume adjusted to {VOLUME_VALUE * 100:.0f}%."
